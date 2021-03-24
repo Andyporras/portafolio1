b172e9d00bcc4dbb4681f9cc42abaa4bd8b2abeb
Andy Porras <80351713+Andyporras@users.noreply.github.com>
2021-03-23 18:07:08 -0600

    Update portafolio1_parte 2.py

--- portafolio1_parte 2.py
+++ portafolio1_parte 2.py
@@ -23,7 +23,14 @@ def pasarAentero_aux(num):
     elif(num%10==0):
         return pasarAentero_aux(num//10)
     else:
-        return num
+        return suma_aux(num,0)
+
+def suma_aux(num,indice):
+    if(num==0):
+        return 0
+    else:
+        suma=num%10*10**indice
+        return suma+suma_aux(num//10,indice+1)
 
 
 #--------------------------------------------------------
@@ -48,8 +55,7 @@ def contarDigitosFlotante(num):
         else:
             return contarDigitosFlotante_aux(num)
     elif(num<0):
-        return contarDigitosFlotante
-    (num*-1)
+        return contarDigitosFlotante(num*-1)
     elif(num%10==0):
         return contarDigitosFlotante(num//10)
     elif(num>0):
@@ -76,20 +82,33 @@ retrincciones:
 indice=numero entero positivo mayor o igual que 0
 num=numero entero mayor que cero 
 """  
-def indiceNumero(num,indice):
-    if(isinstance(num,int)and isinstance(indice,int)and num>0) :
-        #comprobacion de numero entero positivo
-        return indiceNumero_aux(num,indice)
+def indice(num,indice):
+    if(isinstance(num,int)and num>0 and isinstance(indice,int)and indice>=0):
+        largo= largo_aux(num)
+        comparar=num-1
+        return indice_aux(num,indice+1,largo,comparar)
     else:
-        return "digite un numero entero positivo "
+        return "error"
 
-def indiceNumero_aux(num,indice):
-    if(indice>=0):
-        num=str(num)
-        print(int(num[indice]))
+def indice_aux(num,indice,largo,comparar):
+    if(num==0):
+        return 0
+    elif(num<comparar)and num>10:
+        suma=num%10
+        return suma+indice_aux(num%10//10,indice,largo,comparar)
+    elif(num<comparar)and num<10:
+        return num+indice_aux(num//10,indice,largo,comparar)
+    elif(indice==largo):
+        return num%10+indice_aux(num%10//10,indice,largo,comparar)
     else:
-        return
-    
+        largo=largo-indice
+        return indice_aux(num//(10**largo),indice,largo,comparar)
+
+def largo_aux(num):
+    if(num==0):
+        return 0
+    else:
+        return 1+largo_aux(num//10)
 
 #---------------------------------------------------------------
 """
@@ -100,23 +119,21 @@ numeros entero positivo
 salida:
 suma de los numero que se pidio en el indice
 retrincciones:
-num=numero mayor que 0
+num=numero entero mayor que 0
 indice=numero entero positivo mayor o igual que cero
 indice2=numero entero positivo mayor que cero cero
 """
-def contarNumero(num,indice,indice2):
-    if(isinstance(num,int)and isinstance(indice,int)and isinstance(indice2,int)):
-        #comprobacion de numero entero positivo
-        return contarNumero_aux(num,indice,indice2)
+def sumaIndice(num,indice1,indice2):
+    if(isinstance(num,int)and isinstance(indice1,int)and isinstance(indice2,int)and indice1>=0 and indice2>=0):
+        if(num>0):
+            numeroAsumar=indice(num,indice1)
+            numeroAsumar2=indice(num,indice2)
+            suma=numeroAsumar*10+numeroAsumar2
+            return suma+sumaIndice(num%10//10,numeroAsumar,numeroAsumar)
+        else:
+            return 0
     else:
-        return "error,digite un numero entero positivo"
-
-def contarNumero_aux(num,indice,indice2):
-    if(num>0):
-        num=str(num)
-        print(int(num[indice])*10*1+int(num[indice2]))
-    else:
-        return "error,digite un numero entero positivo mayro que cero"
+        "error"
     
     
 
@@ -124,3 +141,5 @@ def contarNumero_aux(num,indice,indice2):
 
 
     
+
+
